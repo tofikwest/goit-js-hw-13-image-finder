289ad525444662d5a5492609e4f94dd8c0946416
Tofik Hasanov <annexcies@gmail.com>
2021-07-04 22:26:13 +0300

    did hw

--- src/apiService.js
+++ src/apiService.js
@@ -1,11 +1,37 @@
-const options = {
-  API_KEY: '22355428-bdc6eaf2c1f0229d27359a629',
-  BASE_URL: 'https://pixabay.com/api/?image_type=photo&orientation=horizontal',
-  BASE_PAGE: '1'
+export default class ApiServices {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async searchPhoto () {
+
+    const options = {
+      API_KEY: '22355428-bdc6eaf2c1f0229d27359a629',
+      BASE_URL: 'https://pixabay.com/api/?image_type=photo&orientation=horizontal',
+    }
+    return fetch(`${options.BASE_URL}&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${options.API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      this.incrementPage()
+      return data.hits;
+    });
+    
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
 }
 
-async function searchPhoto (name = "q=") {
-  const response = await fetch(`${options.BASE_URL}&${name}&page=${options.BASE_PAGE}&per_page=12&key=${options.API_KEY}`);
-  return response.json
-}
-searchPhoto('q=cat').then(console.log)
+
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-// import './sass/main.scss';
-// import data from './JSON/menu.json';
-// import item from './templates/menu.hbs';
+import './sass/main.scss';
+import ApiServices from './apiService';
+import articlesTpl from './templates/articles.hbs';
+const debounce = require('lodash.debounce')
 
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('#loadMore'),
+}
 
+refs.searchForm.addEventListener('input', debounce(onSearch, 500))
+refs.loadMore.addEventListener('click', onLoadMore)
+
+const apiServices = new ApiServices();
+
+function onSearch (e) {
+  apiServices.query = e.target.value;
+  apiServices.resetPage()
+  apiServices.searchPhoto().then(markUpArticles)
+  const element = document.getElementById('loadMore');
+  element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
+  
+}
+
+function onLoadMore() {
+  apiServices.searchPhoto().then(markUpArticles)
+  const element = document.querySelector('.gallery').lastElementChild;
+  element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
+  
+}
+
+function markUpArticles (article) {
+  refs.gallery.insertAdjacentHTML('beforeend', articlesTpl(article))
+}
